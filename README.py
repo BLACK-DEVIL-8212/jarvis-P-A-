438ec9d903eaf0a9828b72f92b262aeaf3fd2932
BLACK-DEVIL-8212 <75551857+BLACK-DEVIL-8212@users.noreply.github.com>
2023-03-10 07:59:55 -0800

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
 # some packages are important for yourb ai program \
-# this program is spetialy for linux opoerating system \
+# this program is spetialy for linux and windows opoerating system \
 
 pip install -r requirements.py
